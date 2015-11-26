5ea3b912bf32bfbbb0d899bbd060c6c38c9ac167
duttaoindril <duttaoindril@gmail.com>
2015-11-25 23:58:32 -0800

    OK BRAH

---
+++ proj-main.asm
@@ -0,0 +1,37 @@
+.include "./cs47_proj_macro.asm"
+.include "./cs47_proj_procs.asm"
+
+# data section
+.data 
+# TBD: Complete your data section
+
+.text
+.globl main
+main:
+# TBD: Complete your main program here
+	exit
+
+
+#####################################################################
+# Implement au_logical
+# Argument:
+# 	$a0: First number
+#	$a1: Second number
+#	$a2: operation code ('+':add, '-':sub, '*':mul, '/':div)
+# Return:
+#	$v0: ($a0+$a1) | ($a0-$a1) | ($a0*$a1):LO | ($a0 / $a1)
+# 	$v1: ($a0 * $a1):HI | ($a0 % $a1)
+# Notes:
+#####################################################################
+
+#####################################################################
+# Implement au_normal
+# Argument:
+# 	$a0: First number
+#	$a1: Second number
+#	$a2: operation code ('+':add, '-':sub, '*':mul, '/':div)
+# Return:
+#	$v0: ($a0+$a1) | ($a0-$a1) | ($a0*$a1):LO | ($a0 / $a1)
+# 	$v1: ($a0 * $a1):HI | ($a0 % $a1)
+# Notes:
+#####################################################################
