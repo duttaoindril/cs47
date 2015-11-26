c5f3783a74138799f004ce02047d16d825136508
duttaoindril <duttaoindril@gmail.com>
2015-11-26 02:19:09 -0800

    deleted useless comments

--- ProjectTBD/proj-main.asm
+++ ProjectTBD/proj-main.asm
@@ -97,28 +97,3 @@ error:
 quit:
 	print_str(prompth)
 	exit
-
-
-#####################################################################
-# Implement au_logical
-# Argument:
-# 	$a0: First number
-#	$a1: Second number
-#	$a2: operation code ('+':add, '-':sub, '*':mul, '/':div)
-# Return:
-#	$v0: ($a0+$a1) | ($a0-$a1) | ($a0*$a1):LO | ($a0 / $a1)
-# 	$v1: ($a0 * $a1):HI | ($a0 % $a1)
-# Notes:
-#####################################################################
-
-#####################################################################
-# Implement au_normal
-# Argument:
-# 	$a0: First number
-#	$a1: Second number
-#	$a2: operation code ('+':add, '-':sub, '*':mul, '/':div)
-# Return:
-#	$v0: ($a0+$a1) | ($a0-$a1) | ($a0*$a1):LO | ($a0 / $a1)
-# 	$v1: ($a0 * $a1):HI | ($a0 % $a1)
-# Notes:
-#####################################################################
